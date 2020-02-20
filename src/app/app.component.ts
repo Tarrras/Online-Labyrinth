e931149187a018b1e0c2e10e85f5e24c999ca3d2
Taras <vtaras15@gmail.com>
2020-02-20 22:15:55 +0200

    now server can have a multiple different clients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,61 +16,73 @@ export class AppComponent implements OnInit {
 
   public ngOnInit() {
     this.socket = io("http://localhost:3000");
-    console.log("connected")
+    
   }
 
   public ngAfterViewInit() {
+    console.log("SSSSSSS")
+
     this.context = this.gameCanvas.nativeElement.getContext("2d");
-    this.socket.on("position", position => {
-      this.context.fillRect(position.x, position.y, 20, 20);
+    this.socket.emit("new player")
+    // this.socket.on("position", position => {
+    //   console.log(position.pos.position.x)
+    //   this.context.fillRect(position.position, position.y, 20, 20);
+    //   switch (position.direction) {
+    //     case "left":
+    //       this.context.clearRect(
+    //         position.pos.position.x + 5,
+    //         position.pos.position.y,
+    //         20,
+    //         20
+    //       );
+    //       this.context.fillRect(position.pos.position.x, position.pos.position.y, 20, 20);
+    //       console.log("y" + position.pos.position.x)
+    //       console.log("x" + position.pos.position.y)
+    //       break;
+    //     case "right":
+    //       this.context.clearRect(
+    //         position.pos.x - 5,
+    //         position.pos.y,
+    //         20,
+    //         20
+    //       );
+    //       console.log("y" + position.pos.x)
+    //       console.log("x" + position.pos.y)
+    //       this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
+    //       break;
+    //     case "up":
+    //       this.context.clearRect(
+    //         position.pos.x,
+    //         position.pos.y + 5,
+    //         20,
+    //         20
+    //       );
+    //       console.log("y" + position.pos.x)
+    //       console.log("x" + position.pos.y)
+    //       this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
+    //       break;
+    //     case "down":
+    //       this.context.clearRect(
+    //         position.pos.x,
+    //         position.pos.y - 5,
+    //         20,
+    //         20
+    //       );
+    //       console.log("y" + position.pos.x)
+    //       console.log("x" + position.pos.y)
+    //       this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
+    //       break;
+    //   }
 
-      switch (position.direction) {
-        case "left":
-          this.context.clearRect(
-            position.pos.x + 5,
-            position.pos.y,
-            20,
-            20
-          );
-          this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
-          console.log("y" + position.pos.x)
-          console.log("x" + position.pos.y)
-          break;
-        case "right":
-          this.context.clearRect(
-            position.pos.x - 5,
-            position.pos.y,
-            20,
-            20
-          );
-          console.log("y" + position.pos.x)
-          console.log("x" + position.pos.y)
-          this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
-          break;
-        case "up":
-          this.context.clearRect(
-            position.pos.x,
-            position.pos.y + 5,
-            20,
-            20
-          );
-          console.log("y" + position.pos.x)
-          console.log("x" + position.pos.y)
-          this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
-          break;
-        case "down":
-          this.context.clearRect(
-            position.pos.x,
-            position.pos.y - 5,
-            20,
-            20
-          );
-          console.log("y" + position.pos.x)
-          console.log("x" + position.pos.y)
-          this.context.fillRect(position.pos.x, position.pos.y, 20, 20);
-          break;
-      }
+    // })
 
+    this.socket.on('state', players => {
+        this.context.clearRect(0, 0 , 800, 600)
+        this.context.fillStyle = 'green'
+        for(var id in players){
+          var player = players[id];
+          this.context.fillRect(player.x, player.y, 20, 20);
+        }
     })
   }
 
