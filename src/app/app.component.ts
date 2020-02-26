7130fa5bc3ceba676af933393e97a1b6137f51af
Taras <vtaras15@gmail.com>
2020-02-26 18:48:37 +0200

    block moving without lags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 import io from "socket.io-client";
+
+
+enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40
+}
 
 @Component({
   selector: 'app-root',
@@ -11,7 +19,26 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild("game")
   private gameCanvas: ElementRef;
 
+  @HostListener('document:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.keyCode) {
+      case KEY_CODE.RIGHT_ARROW:
+        this.move("right")
+        break;
+      case KEY_CODE.LEFT_ARROW:
+        this.move("left")
+        break;
+      case KEY_CODE.UP_ARROW:
+        this.move("up")
+        break;
+      case KEY_CODE.DOWN_ARROW:
+        this.move("down")
+        break;
+    }
+  }
 
+
+  private canvas: any;
   private context: any;
   private socket: any;
   private board: any;
@@ -20,11 +47,9 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   public ngOnInit() {
     this.socket = io("http://localhost:3000");
-
   }
 
   public ngAfterViewInit() {
-
     this.context = this.gameCanvas.nativeElement.getContext("2d");
     this.socket.emit("new player")
 
@@ -33,21 +58,30 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.draw()
     })
 
+    this.socket.on('old', oldPosition => {
+      this.context.clearRect(oldPosition.x, oldPosition.y, 20, 20)
+    })
+
     this.socket.on('state', players => {
       // this.context.clearRect(0, 0, 800, 600)
+      // this.draw()
       this.context.fillStyle = this.colors[1]
       for (var id in players) {
         var player = players[id];
-        this.context.fillRect(player.x, player.y, 20, 20);
+        // this.context.fillRect(player.x, player.y, 20, 20);
+        this.context.beginPath();
+        this.context.rect(player.x, player.y, 20, 20);
+        this.context.fillStyle = 'red';
+        this.context.fill();
+        this.context.closePath();
       }
     })
 
-    
+
   }
 
   public draw() {
     var width = 640
-    console.log(this.board)
     var blockSize = width / this.board.length;
     this.context.setTransform(1, 0, 0, 1, 0, 0);
     this.context.clearRect(0, 0, width, width);
@@ -57,10 +91,9 @@ export class AppComponent implements OnInit, AfterViewInit {
       for (var x = 0; x < this.board[y].length; x++) {
         //Draw a wall
         if (this.board[y][x] === 1) {
-          console.log(this.board[y][x])
           this.context.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
         }
-        //Draw the goal
+        // Draw the goal
         // else if (this.board[y][x] === -1) {
         //   this.context.beginPath();
         //   this.context.lineWidth = 5;
